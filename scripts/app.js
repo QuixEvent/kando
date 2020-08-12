6adf9da786ee8f6184240ffaa991a911ac3811f0
Frantisek Rapcak <frantisek.rapcak@gmail.com>
2020-08-12 10:10:11 +0200

    init project

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+console.log('Hallow 🙏 World');
+
+
+//
+//  DOM ready
+//
+//  console.log('DOM ——— ready');
+//});
+
+
+
+
+//
+//  window RESIZE
+//
+//$(window).resize(function () {
+//  console.log('window ——— resized');
+//});
